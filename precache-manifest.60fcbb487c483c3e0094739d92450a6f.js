4e1f420946434153d167d72d1988593a8ed8a50e
- <>
2020-12-27 11:55:41 +0330

    first publish

---
+++ precache-manifest.60fcbb487c483c3e0094739d92450a6f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801b390d7e1ac67741e4685d6942df45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9a7fcb4fddcd15a7ee3",
+    "url": "/static/css/main.787c1f45.chunk.css"
+  },
+  {
+    "revision": "a41fa5210cb4154cde2e",
+    "url": "/static/js/2.d31a9f15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d31a9f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a7fcb4fddcd15a7ee3",
+    "url": "/static/js/main.7600dd78.chunk.js"
+  },
+  {
+    "revision": "b16bc59858fa6210dd4c",
+    "url": "/static/js/runtime-main.ca29c466.js"
+  },
+  {
+    "revision": "a73d24769c0b06c6d9860daf5a15aeb3",
+    "url": "/static/media/0-be-asani.a73d2476.svg"
+  },
+  {
+    "revision": "95491ec991ab7813b1cf9a08fc27591b",
+    "url": "/static/media/1-ba-sooraat.95491ec9.svg"
+  },
+  {
+    "revision": "9982c55956394231e8a23ccac7cfe202",
+    "url": "/static/media/2-hooshmandane.9982c559.svg"
+  },
+  {
+    "revision": "6c42ea9f3e233d9110e04dcd87db55da",
+    "url": "/static/media/3-eghtesadi.6c42ea9f.svg"
+  },
+  {
+    "revision": "21c832e86d9f919dd2dcb24f554f669f",
+    "url": "/static/media/4-elmi.21c832e8.svg"
+  },
+  {
+    "revision": "c9b563417a1ff01700edc358b5fc309f",
+    "url": "/static/media/5-ba-movafaghiat.c9b56341.svg"
+  },
+  {
+    "revision": "91c005d9108b18392832578533e6f884",
+    "url": "/static/media/6-ghavi.91c005d9.svg"
+  },
+  {
+    "revision": "99ee25afc79bd853311f019eddc53ab4",
+    "url": "/static/media/7-motabegh-meil.99ee25af.svg"
+  },
+  {
+    "revision": "2f04073020feab26f94de654cbb8df50",
+    "url": "/static/media/8-herfeii.2f040730.svg"
+  },
+  {
+    "revision": "05fc0db7ff36fd3bd9643e2904f7534a",
+    "url": "/static/media/Check.05fc0db7.svg"
+  },
+  {
+    "revision": "a990b8333761ecf7bafb5e9ac9145e1a",
+    "url": "/static/media/Home.a990b833.png"
+  },
+  {
+    "revision": "2525083d408346967f93eba457ac5827",
+    "url": "/static/media/ZahraRoosta.2525083d.ttf"
+  },
+  {
+    "revision": "1b3712bddbcb3c51ec915bd99d7355f7",
+    "url": "/static/media/ali.1b3712bd.svg"
+  },
+  {
+    "revision": "508e484aa259dadc3e0710228b3ee114",
+    "url": "/static/media/contact.508e484a.png"
+  },
+  {
+    "revision": "9de6dc7ab28f430ffc46439b25e6a303",
+    "url": "/static/media/email-icon.9de6dc7a.svg"
+  },
+  {
+    "revision": "36015921a7e1b9b80b2368da071c72bc",
+    "url": "/static/media/mohsen.36015921.svg"
+  },
+  {
+    "revision": "2efe8a9283312f277771d8f85c6bf04c",
+    "url": "/static/media/p1.2efe8a92.png"
+  },
+  {
+    "revision": "b43316ac2887a8d0a93ea37714a0f02a",
+    "url": "/static/media/p2.b43316ac.png"
+  },
+  {
+    "revision": "bd36fdaf6b8416594c0c621025655bbe",
+    "url": "/static/media/p3.bd36fdaf.png"
+  },
+  {
+    "revision": "bf9403f3223ce93796bc95087978e1c6",
+    "url": "/static/media/p4.bf9403f3.png"
+  },
+  {
+    "revision": "739e236e04f24e63dc2596e236d0f9e6",
+    "url": "/static/media/phone-icon.739e236e.svg"
+  },
+  {
+    "revision": "e371c095488897cb48bd1ce32b2d32a9",
+    "url": "/static/media/sara.e371c095.svg"
+  }
+]);
